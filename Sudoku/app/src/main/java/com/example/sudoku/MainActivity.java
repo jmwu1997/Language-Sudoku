235c6224743e084abbc1d496ceffc00390040d28
yuenkay <yuenkay@sfu.ca>
2019-01-31 13:27:01 -0800

    updated Sudoku

--- Sudoku/app/src/main/java/com/example/sudoku/MainActivity.java
+++ Sudoku/app/src/main/java/com/example/sudoku/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableLayout tblLayout = (TableLayout)findViewById(R.id.tableLayout);
+
+        for(int i=0;i<9;i++)
+        {
+            TableRow row = (TableRow)tblLayout.getChildAt(i);
+            for(int j=0;j<9;j++){
+                Button button = (Button)row.getChildAt(j); // get child index on particular row
+                String buttonText = button.getText().toString();
+                Log.i("Button index: "+(i+j), buttonText);
+            }
+        }
     }
+
+
+
 }
