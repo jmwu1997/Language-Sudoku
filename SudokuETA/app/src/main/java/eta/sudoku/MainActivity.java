57b1f1dc72b56b5ceade926888df53b4e5ccb16e
yuenkay <yuenkay@sfu.ca>
2019-02-01 13:48:07 -0800

    rebuilt project

---
+++ SudokuETA/app/src/main/java/eta/sudoku/MainActivity.java
@@ -0,0 +1,29 @@
+package eta.sudoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TableLayout tblLayout = (TableLayout)findViewById(R.id.tableLayout);
+
+        for(int i=0;i<9;i++)
+        {
+            TableRow row = (TableRow)tblLayout.getChildAt(i);
+            for(int j=0;j<9;j++){
+                Button button = (Button)row.getChildAt(j); // get child index on particular row
+                String buttonText = button.getText().toString();
+                Log.i("Button index: "+(i+j), buttonText);
+            }
+        }
+    }
+}
