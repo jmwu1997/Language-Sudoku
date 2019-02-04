45900492ba5f55750f9d01f141dd8430f1fd9b6e
jza221 <jza221@sfu.ca>
2019-02-03 21:13:56 -0800

    Vocab ver1.1

--- SudokuETA/app/src/main/java/eta/sudoku/Vocab.java
+++ SudokuETA/app/src/main/java/eta/sudoku/Vocab.java
@@ -2,8 +2,8 @@ package eta.sudoku;
 
 public class Vocab {
     //Class of words in different languages with same meaning
-    private int mNumLang=2; //# of languages(unused)
-    private String mWords[]; //an array of words w/ same meaning
+    private int mNumLang=2; //# of languages
+    private String mWords[] = new String[mNumLang]; //an array of words w/ same meaning
 
     public Vocab(String[] words){ setWords(words);} //construct the class with a complete tuple of same meaning words
     public Vocab() {} //or set words later
@@ -12,5 +12,6 @@ public class Vocab {
     public void setWords(String[] words) { mWords = words; } //set a tuple
     public String getWord(int lang) { return mWords[lang]; } //get the word in specific language with language index
     public void setWord(int lang, String word){ mWords[lang] = word; } //set a word in specific language with language index
+
 
 }
