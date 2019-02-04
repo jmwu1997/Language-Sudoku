96e3b3d62b0900bc0738d94f9e36e9b2a259094e
jza221 <jza221@sfu.ca>
2019-02-03 21:31:52 -0800

    Puzzle.java ver1.1: added answer checking

--- SudokuETA/app/src/main/java/eta/sudoku/Puzzle.java
+++ SudokuETA/app/src/main/java/eta/sudoku/Puzzle.java
@@ -1,12 +1,20 @@
 package eta.sudoku;
 
-import android.util.Log;
+
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+
+import java.util.Arrays;
+
+
+
 public class Puzzle {
+    // 1-9 for words, 0 for blank
     private int[][] mPuzzle;
+    //private int[] mRange = {1,2,3,4,5,6,7,8,9};
+    private Integer[] mRange = {1,2,3,4,5,6,7,8,9};
 
     public Puzzle(int[][] savedPuzzle){//construct with a pre-generated puzzle
         createPuzzle(savedPuzzle);
@@ -18,29 +26,18 @@ public class Puzzle {
     public void createPuzzle(int[][] savedPuzzle){//create puzzle with a pre-generated puzzle(number ranging from 1-9, 0 for blank
         mPuzzle = savedPuzzle;
     }
+
     public boolean isBoxEmpty(int row, int col){//check if (row,col) box is blank
         if(mPuzzle[row][col]==0) return true;
         else return false;
     }
     public void putWord(int wordIndex, int row, int col){//put specific word into position(row,col) using its index from vocab library
-        //put 1-9 for words, 0 for blank
         //Assumed: the position is for user to put a word into, not for the given puzzle
         //it's not checking for validity
         mPuzzle[row][col] = wordIndex;
     }
-    /*
-    public void genPuzzle(Vocab[] vocabs, int initLang){//for generating test puzzle in console
-        System.out.println("test puzzle");
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++) {
-                System.out.print(vocabs[mPuzzle[i][j]].getWord(initLang));
-            }
-            System.out.println();
-        }
-    }
-    */
-    public void genPuzzleDevice(Vocab[] vocabs, int initLang, TableLayout table){//for gen test puzzle on device
-        //!!!method name will be changed later!!!
+
+    public void genPuzzle(Vocab[] vocabs, int initLang, TableLayout table){//gen test puzzle
         for(int i=0;i<9;i++){
             TableRow mTblRow = (TableRow)table.getChildAt(i); //get table row element
             for(int j=0;j<9;j++) {
@@ -48,5 +45,74 @@ public class Puzzle {
                 mButton.setText(vocabs[mPuzzle[i][j]].getWord(initLang)); //write word on the button at position(i,j) from vocabs in "initial" language used for the puzzle
             }
         }
+    }
+
+    public boolean isCompleted(){ //check if all the cells are filled
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++) {
+                if(isBoxEmpty(i,j)) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isRowSolved(int row){
+        assert row>=0 & row<9;
+        //convert int[] to Integer[]
+        Integer[] mRow = new Integer[9];
+        for(int i=0; i<9; i++){
+            mRow[i] = Integer.valueOf(mPuzzle[row][i]);
+        }
+        if( !Arrays.asList(mRow).containsAll(Arrays.asList(mRange)) ){ //mRow and mRange have to be Integer[]
+            System.out.print("row ");
+            System.out.print(row);
+            System.out.println(" unsolved");
+            System.out.println(mPuzzle[row]);
+            return false;
+        }
+        return true;
+    }
+    private boolean isColSolved(int col) {
+        assert col>=0 & col<9;
+
+        Integer[] mCol = new Integer[9];
+        for(int i=0; i<9; i++){
+            mCol[i] = Integer.valueOf(mPuzzle[i][col]);
+        }
+        if( !Arrays.asList(mCol).containsAll(Arrays.asList(mRange)) ){
+            System.out.print("col ");
+            System.out.print(col);
+            System.out.println(" unsolved");
+            return false;
+        }
+        return true;
+    }
+    private boolean isSubSolved(int sub){
+        //sub index:
+        //0 1 2
+        //3 4 5
+        //6 7 8
+        assert sub>=0 & sub<9;
+
+        Integer[] mSub = new Integer[9];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                mSub[i*3+j] = Integer.valueOf(mPuzzle[(sub/3)*3+i][(sub%3)*3+j]);
+            }
+        }
+        if( !Arrays.asList(mSub).containsAll(Arrays.asList(mRange)) ){
+            System.out.print("sub ");
+            System.out.print(sub);
+            System.out.println(" unsolved");
+            return false;
+        }
+        return true;
+    }
+    public boolean isSolved(){
+        if(!isCompleted()) return false;
+        for(int i=0; i<9; i++){
+            if(!isRowSolved(i) || !isColSolved(i) || !isSubSolved(i)) return false;
+        }
+        return true;
     }
 }
